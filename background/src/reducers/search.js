be2da426ba0dd66d5c77be41e2901a79fb46830a
Dang Dien PHAN <ddphan@linagora.com>
2019-01-14 20:48:35 +0700

    build for production

--- background/src/reducers/search.js
+++ background/src/reducers/search.js
@@ -1,6 +1,6 @@
 import { SEARCH_TYPES } from '../constants/search';
 import { SEARCH as actionTypes } from '../constants/actionTypes'
-import { union } from 'lodash'
+import union from 'lodash/union'
 
 const defaultState = {
   selectedSeachMenuBarItem: SEARCH_TYPES.TRACKS,
