b224d2adbf83e01a2a84f94841ea552284e30274
Dang Dien PHAN <ddphan@linagora.com>
2019-04-27 17:32:02 +0700

    add a helper about ads mute feature on Web player

--- popup/src/components/NoDevice.js
+++ popup/src/components/NoDevice.js
@@ -11,7 +11,8 @@ const NoDevice = () => {
   return(
     <div className="no-device">
       <span>No Device Found !</span>
-      <span className="no-device-helper">This version only support Web and Desktop players</span>
+      <span className="no-device-helper">This version support only Web and Desktop players</span>
+      <span className="no-device-helper" style={{fontWeight: 800}}>Open Web player to mute ads</span>
       <button onClick={onOpenPlayer}>Open web player</button>
     </div>
   )
